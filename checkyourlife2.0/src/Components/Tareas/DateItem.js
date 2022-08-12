748356f89c58e8950b79683f6b004d5e6a9707d8
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-08-11 22:23:41 -0600

    refactor To Do List dates structures.

--- checkyourlife2.0/src/Components/Tareas/DateItem.js
+++ checkyourlife2.0/src/Components/Tareas/DateItem.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Button, ButtonGroup } from '@chakra-ui/react'
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 
-function DateItem({data}) {
+function DateItem({data, selected, onClickFunction}) {
+  data = data.split(' ');
+
 
   return (
     <>
-        <div className="date_item_container">
-            {data}
+        <div className={'date_item_container' + (selected? ' selected_date': '')} onClick={()=> {onClickFunction(data[1])}}>
+            {data[0]} <br /> {data[1]}
         </div>
     </>
   );
--- checkyourlife2.0/src/Pages/Tareas.js
+++ checkyourlife2.0/src/Pages/Tareas.js
@@ -1,50 +1,81 @@
 import React, { useState, useEffect } from "react";
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, ButtonGroup, Grid, GridItem } from '@chakra-ui/react'
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import { IconButton } from '@chakra-ui/react'
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons'
 import '../Assets/Styles/Tareas/Page.scss'
 import TaskItem from "../Components/Tareas/TaskItem";
 import DateItem from "../Components/Tareas/DateItem";
-import { getCurrentDates } from "../Services/Tareas";
+import { convertDate, getCurrentDates } from "../Services/Tareas";
 
 function Tareas() {
   const dates = getCurrentDates();
-  const [data, setData] = useState([1,2,3,4,5,6,7,8,9,10]);
+  const [selectedDate, setSelectedDate] = useState(0);
+  const [data, setData] = useState([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
   const [scroll, setScroll] = useState(0);
   const navigate = useNavigate();
+
+  const selectDate = (date) => {
+    setSelectedDate(date)
+  }
 
 
   return (
     <>
-        <div className="task_page_general_container">
-          <div className="tasks_container">
-            <div className="task_header">
-              <div className="dates_carousel" >
-                <div className="dates_inner_carousel"  style={{ transform: `translateX(-${scroll}%)`}}>
-                {
-                  dates.map((day) => (
-                    <DateItem key={day} data={day.getDate()}></DateItem>
-                  ))
-                }
+      <Grid
+        h='100%'
+        templateRows='repeat(2, 1fr)'
+        templateColumns='repeat(2, 1fr)'
+        gap={10}
+      >
+        <GridItem rowSpan={1}> 
+          <div className="search_task_panel">
+
+          </div>
+        </GridItem>
+        <GridItem rowSpan={2} colSpan={1}>
+          <div className="task_page_general_container">
+            <div className="tasks_container">
+              <div className="task_header">
+                <div className="dates_carousel" >
+                  <div className="dates_inner_carousel"  style={{ transform: `translateX(-${scroll}%)`}}>
+                  {
+                    dates.map((day) => (
+                      <DateItem 
+                        key={day} 
+                        data={convertDate(day.getDay(),day.getDate())} 
+                        selected={selectedDate == day.getDate()} 
+                        onClickFunction={selectDate}
+                        ></DateItem>
+                        ))
+                      }
+                  </div>
+                </div>
+                <div className="arrows_container">
+                  <div className="left_arrow_container">
+                    <IconButton className="left_arrow" aria-label='Search database' colorScheme='blackAlpha' size='sm' icon={<ChevronLeftIcon />} onClick={() => {dates.length > 28? scroll === 0 ? setScroll(scroll) : setScroll(scroll-100) : scroll === 0? setScroll(0) : setScroll(scroll-100) }}/>
+                  </div>
+                  <div className="right_arrow_container">
+                    <IconButton className="right_arrow" aria-label='Search database' colorScheme='blackAlpha' size='sm' icon={<ChevronRightIcon />} onClick={() => {dates.length > 28? scroll === 400? setScroll(scroll) : setScroll(scroll+100) : scroll === 300? setScroll(scroll) : setScroll(scroll+100) }} />
+                  </div>
                 </div>
               </div>
-              <div className="arrows_container">
-                <div className="left_arrow_container">
-                  <IconButton className="left_arrow" aria-label='Search database' colorScheme='blackAlpha' size='sm' icon={<ChevronLeftIcon />} onClick={() => {dates.length > 28? scroll === 0 ? setScroll(scroll) : setScroll(scroll-100) : scroll === 0? setScroll(0) : setScroll(scroll-100) }}/>
-                </div>
-                <div className="right_arrow_container">
-                  <IconButton className="right_arrow" aria-label='Search database' colorScheme='blackAlpha' size='sm' icon={<ChevronRightIcon />} onClick={() => {dates.length > 28? scroll === 500? setScroll(scroll) : setScroll(scroll+100) : scroll === 400? setScroll(scroll) : setScroll((scroll+100)%400) }} />
-                </div>
+              <div className="daily_tasks_container">
+                {
+                  data.map((task) => (
+                    <TaskItem key={task} data={task} />
+                    ))
+                  }
               </div>
             </div>
-            {
-              data.map((task) => (
-                <TaskItem key={task} data={task} />
-              ))
-            }
+          </div>  
+        </ GridItem>
+        <GridItem rowSpan={1}> 
+          <div className="add_task_panel">
+
           </div>
-        </div>
+        </GridItem>
+      </Grid>
     </>
   );
 }
