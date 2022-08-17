0a3a0433ce91e8254a74c539d63000e70a36895b
KennethGalvez <gal20079@uvg.edu.gt>
2022-08-16 18:29:56 -0600

    Css + Funciones base para cronometro

--- checkyourlife2.0/src/Pages/Cronometro.js
+++ checkyourlife2.0/src/Pages/Cronometro.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { Button, ButtonGroup } from '@chakra-ui/react';
+import '../Components/Cronometro.css';
 import {
   Route, Routes, useLocation, useNavigate,
 } from 'react-router-dom';
@@ -8,10 +9,30 @@ import {
 function Cronometro() {
   const navigate = useNavigate();
 
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+
+  let timer;
+  useEffect(() => {
+    timer = setInterval(() => {
+      setSeconds(seconds + 1);
+      if (seconds === 59) {
+        setMinutes(minutes + 1);
+        setSeconds(0);
+      }
+    }, 1000);
+    return () => clearInterval(timer);
+  });
+
   return (
-    <>
-      Cronometro
-    </>
+    <div className="timer">
+      <div className="container">
+        <div className="timer_container">
+          <h1>Cronometro</h1>
+          <h1>10:30</h1>
+        </div>
+      </div>
+    </div>
   );
 }
 
