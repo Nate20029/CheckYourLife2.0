d1a51771d27f6b12c683ec28a1a7c0426f506e10
KennethGalvez <gal20079@uvg.edu.gt>
2022-08-17 00:02:09 -0600

    Avance en funcionamiento

--- checkyourlife2.0/src/Pages/Cronometro.js
+++ checkyourlife2.0/src/Pages/Cronometro.js
@@ -1,35 +1,76 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { Button, ButtonGroup } from '@chakra-ui/react';
-import '../Components/Cronometro.css';
 import {
   Route, Routes, useLocation, useNavigate,
 } from 'react-router-dom';
+import '../Components/Cronometro.css';
+import DisplayComponent from '../Components/DisplayComponent';
+import BtnComponent from '../Components/BtnComponent';
 
 function Cronometro() {
   const navigate = useNavigate();
 
-  const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-
-  let timer;
-  useEffect(() => {
-    timer = setInterval(() => {
-      setSeconds(seconds + 1);
-      if (seconds === 59) {
-        setMinutes(minutes + 1);
-        setSeconds(0);
-      }
-    }, 1000);
-    return () => clearInterval(timer);
+  const [time, setTime] = useState({
+    ms: 0, s: 0, m: 0, h: 0,
   });
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+  // Not started = 0
+  // started = 1
+  // stopped = 2
+
+  let updatedMs = time.ms;
+  let updatedS = time.s;
+  let updatedM = time.m;
+  let updatedH = time.h;
+
+  const run = () => {
+    if (updatedM === 60) {
+      updatedH += 1;
+      updatedM = 0;
+    }
+    if (updatedS === 60) {
+      updatedM += 1;
+      updatedS = 0;
+    }
+    if (updatedMs === 100) {
+      updatedS += 1;
+      updatedMs = 0;
+    }
+    updatedMs += 1;
+    return setTime({
+      ms: updatedMs, s: updatedS, m: updatedM, h: updatedH,
+    });
+  };
+
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({
+      ms: 0, s: 0, m: 0, h: 0,
+    });
+  };
+
+  const resume = () => start();
 
   return (
-    <div className="timer">
-      <div className="container">
-        <div className="timer_container">
-          <h1>Cronometro</h1>
-          <h1>10:30</h1>
+    <div className="main-section">
+      <div className="clock-holder">
+        <div className="stopwatch">
+          <DisplayComponent time={time} />
+          <BtnComponent status={status} resume={resume} reset={reset} stop={stop} start={start} />
         </div>
       </div>
     </div>
