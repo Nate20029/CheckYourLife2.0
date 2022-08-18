61961ee4dd3b63cf06766b5028ad38485634d3af
Natanael Girón <70552363+Nate20029@users.noreply.github.com>
2022-08-18 01:56:04 -0600

    Pantalla principal comunidad

--- checkyourlife2.0/src/Pages/Comunidad.js
+++ checkyourlife2.0/src/Pages/Comunidad.js
@@ -4,14 +4,26 @@ import { Button, ButtonGroup } from '@chakra-ui/react';
 import {
   Route, Routes, useLocation, useNavigate,
 } from 'react-router-dom';
+import { ChatEngine } from 'react-chat-engine';
+import '../Components/comunidad.css';
 
 function Comunidad() {
   const navigate = useNavigate();
 
   return (
-    <>
-      Comunidad
-    </>
+    <div className="chats-page">
+      <div className="nav-bar">
+        <div className="logo-tab">
+          Comunidad
+        </div>
+      </div>
+      <ChatEngine
+        height="calc(100vh - 66px)"
+        userName="Nate"
+        userSecret="barcaroly"
+        projectID="9b86604a-2321-4d5f-a287-f1275a2bbd0e"
+      />
+    </div>
   );
 }
 
