d8f96b34750202322f6ef3a022f9f4e2e9340f3f
Natanael Girón <70552363+Nate20029@users.noreply.github.com>
2022-08-22 16:10:32 -0600

    arreglos de git

---
+++ checkyourlife2.0/src/Components/Sidebar.js
@@ -0,0 +1,58 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {
+  Avatar, Flex, Text, Button,
+} from '@chakra-ui/react';
+import './Sidebar.css';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { addDoc, collection } from '@firebase/firestore';
+import { auth, db } from '../Services/firebase';
+import getOtherEmail from './getOtherEmail';
+
+export default function Sidebar() {
+  const [user] = useAuthState(auth);
+  const [snapshot] = useCollection(collection(db, 'chats'));
+  const chat = snapshot?.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+  const redirect = (id) => {
+    console.log(`esta es una prueba${id}`);
+    const prueba = router.push(`Home/chats/${id}`);
+    console.log(prueba);
+  };
+
+  const newChat = async () => {
+    const input = prompt('Enter email of chat recipient');
+    await addDoc(collection(db, 'chats'), { users: [user.email, input] });
+    console.log(user.email);
+  };
+
+  const chatList = () => (
+    chat?.filter((chats) => chats.users.includes(user.email))
+      .map(
+        (chats) => (
+          <Flex className="usuarios" onClick={() => redirect(chats.id)}>
+            <Avatar className="avatarchat" src="" />
+            <Text className="textusuarios">{getOtherEmail(chats.users, user)}</Text>
+          </Flex>
+        ),
+      )
+  );
+
+  return (
+    <Flex className="todo">
+      <Flex className="arriba2">
+        <Flex className="avatarflex">
+          <Avatar className="avatar" src={user.photoURL} />
+          <Text className="textusuarios">{user.displayName || user.email}</Text>
+        </Flex>
+      </Flex>
+
+      <Button className="button" onClick={() => newChat()}>New Chat</Button>
+
+      <Flex className="chatflex">
+        {chatList()}
+      </Flex>
+
+    </Flex>
+  );
+}
--- checkyourlife2.0/src/Pages/Comunidad.js
+++ checkyourlife2.0/src/Pages/Comunidad.js
@@ -1,17 +1,71 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
-import { Button, ButtonGroup } from '@chakra-ui/react';
 import {
-  Route, Routes, useLocation, useNavigate,
-} from 'react-router-dom';
+  Avatar, Button, Text, Flex, Heading, Input, FormControl,
+} from '@chakra-ui/react';
+import '../Components/comunidad.css';
+import { useCollectionData, useDocumentData } from 'react-firebase-hooks/firestore';
+import { collection, orderBy, query } from '@firebase/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import Sidebar from '../Components/Sidebar';
+import { db, auth } from '../Services/firebase';
+
+function Bottombar() {
+  return (
+    <FormControl className="bottombar">
+      <Input placeholder="Type a message..." autoComplete="off" />
+      <Button type="submit" hidden> Submit</Button>
+    </FormControl>
+  );
+}
+
+function Topbar() {
+  return (
+    <Flex className="topbar">
+      <Avatar className="avatarchatcom" src="" />
+      <Heading size="lg">gal20079@uvg.edu.gt</Heading>
+    </Flex>
+  );
+}
 
 function Comunidad() {
-  const navigate = useNavigate();
-
   return (
-    <>
-      Comunidad
-    </>
+    <Flex className="general">
+
+      <Sidebar />
+
+      <Flex className="mensajesflex">
+        <Topbar />
+
+        <Flex className="chatarea">
+
+          <Flex className="messagess">
+            <Text>Que onda Kenny</Text>
+          </Flex>
+          <Flex className="messagess">
+            <Text>Prueba :p</Text>
+          </Flex>
+          <Flex className="messagesr">
+            <Text>Ola ola</Text>
+          </Flex>
+          <Flex className="messagesr">
+            <Text>Probando</Text>
+          </Flex>
+          <Flex className="messagesr">
+            <Text>Hola</Text>
+          </Flex>
+          <Flex className="messagess">
+            <Text>Buenas Tardes</Text>
+          </Flex>
+          <Flex className="messagess">
+            <Text>Alguien aqui?</Text>
+          </Flex>
+
+        </Flex>
+
+        <Bottombar />
+      </Flex>
+    </Flex>
   );
 }
 
