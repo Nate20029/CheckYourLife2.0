d1a0397143eca7ca1c82ba2bfee9afa55ddd8218
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2022-08-21 19:18:16 -0600

    Cambios a Perfil

--- checkyourlife2.0/src/Pages/Perfil.js
+++ checkyourlife2.0/src/Pages/Perfil.js
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
 import { auth } from '../Services/firebase.js';
+import '../Components/Perfil.css';
 
 function Perfil() {
   const navigate = useNavigate();
@@ -16,9 +17,56 @@ function Perfil() {
     navigate('/');
   }
   return (
-    <div className="Sign-Out">
-      <button type="button" onClick={handleSignOut}>SignOut</button>
-    </div>
+    <>
+      <div className="content-profile-page">
+        <div className="profile-user-page card">
+          <div className="img-user-profile">
+            <img className="profile-bgHome" src="https://37.media.tumblr.com/88cbce9265c55a70a753beb0d6ecc2cd/tumblr_n8gxzn78qH1st5lhmo1_1280.jpg" alt="profile-bgHome" />
+            <img className="avatar" src="http://gravatar.com/avatar/288ce55a011c709f4e17aef7e3c86c64?s=200" alt="jofpin" />
+          </div>
+          <button type="button">Follow</button>
+          <div className="user-profile-data">
+            <h1>Jose Pino</h1>
+            <p>github.com/jofpin</p>
+          </div>
+          <div className="description-profile">
+            Front-end | Security Researcher | CSS Warrior |
+            <a href="https://twitter.com/bullgit" title="bullgit"><strong>@bullgit</strong></a>
+            {' '}
+            | I love to create small things for the internet!
+          </div>
+          <ul className="data-user">
+            <li>
+              <a>
+                <strong>3390</strong>
+                <p1>Posts</p1>
+              </a>
+            </li>
+            <li>
+              <a>
+                <strong>718</strong>
+                <p2>Followers</p2>
+              </a>
+            </li>
+            <li>
+              <a>
+                <strong>239</strong>
+                <p3>Following</p3>
+              </a>
+            </li>
+          </ul>
+          <div className="Sign-Out">
+            <button type="button" onClick={handleSignOut}>SignOut</button>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <h4>
+          Realease 1.0.
+          <a href="https://twitter.com/jofpin" target="_blank" title="José Pino" rel="noreferrer">@Check-Your-Life</a>
+        </h4>
+      </footer>
+    </>
   );
 }
 
