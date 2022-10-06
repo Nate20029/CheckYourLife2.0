0f1b478afaa31cc7879524858b771ab90be3f927
Natanael Girón <70552363+Nate20029@users.noreply.github.com>
2022-10-05 18:34:14 -0600

    test en pagina chat

--- checkyourlife2.0/src/Services/Chat.js
+++ checkyourlife2.0/src/Services/Chat.js
@@ -4,44 +4,4 @@ import { auth, db } from './firebase';
 export const getDataMessenger = async () => {
   const docRef = collection(db, 'usuarios');
   const docSnap = await getDoc(docRef);
-  try {
-    return (docSnap.data());
-  } catch (error) {
-    return error;
-  }
 };
-
-/* export const selecUser = (user) => {
-    setChat(user);
-    console.log(user);
-
-    const user2 = user.uid;
-    // eslint-disable-next-line no-unsafe-optional-chaining
-    const id = usuario?.uid > user2 ? `${usuario?.uid + user2}` : `${user2 + usuario?.uid}`;
-
-    const msgsRef = collection(db, 'messages', id, 'chat');
-    const q = query(msgsRef, orderBy('createdAt', 'asc'));
-
-    onSnapshot(q, (querySnapshot) => {
-      const msgss = [];
-      querySnapshot.forEach((doc) => {
-        msgss.push(doc.data());
-      });
-      setMsgs(msgss);
-    });
-  }; */
-
-/*   export const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const user2 = chat.uid;
-    const id = usuario?.uid > user2 ? `${usuario?.uid + user2}` : `${user2 + usuario?.uid}`;
-
-    await addDoc(collection(db, 'messages', id, 'chat'), {
-      text,
-      from: usuario?.uid,
-      to: user2,
-      createdAt: Timestamp.fromDate(new Date()),
-    });
-    setText('');
-  };  */
--- checkyourlife2.0/src/tests/chat.test.js
+++ checkyourlife2.0/src/tests/chat.test.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { getDataMessenger } from '../Services/Chat';
 
 const ids = 'YbS6L6GN99Xob0hdKe6bnrkfwA634FPh3nzdnEfCQagSYL3KKZfu3uq1';
@@ -6,16 +5,5 @@ const idcorr = 'YbS6L6GN99Xob0hdKe6bnrkfwA634FPh3nzdnEfCQagSYL3KKZfu3uq1';
 
 test('regresa el numero de mensajes existentes: ', async () => {
   const comunidad = await getDataMessenger();
-  expect(comunidad.length).toBe(2);
+  expect(comunidad.lenght).toContain(3);
 });
-
-/* test('regresa undefined por un mal valor: ', async () => {
-  const comunidad = getDataMessenger();
-  expect(comunidad.length).toBe(undefined);
-}); */
-
-/* test('no funciona si está mal el id del mensaje: ', async () => {
-  const comunidad = getDataMessenger(ids);
-  expect(comunidad.length).toBe(undefined);
-});
- */
