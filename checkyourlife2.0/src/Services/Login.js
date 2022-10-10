fc96ce45d6e5ed66bf1964a1ed849d349ad71266
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2022-10-09 18:19:56 -0600

    Cambios de Pruebas

---
+++ checkyourlife2.0/src/Services/Login.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-unused-vars */
+import {
+  signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged,
+  sendPasswordResetEmail, signInWithPopup, GoogleAuthProvider, GithubAuthProvider,
+  FacebookAuthProvider,
+} from 'firebase/auth';
+import { auth } from './firebase.js';
+import {email, password} from '../Pages/Login'
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const handleSignUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredentials) => {
+        const { user } = userCredentials;
+        console.log('Registered with:', user.email);
+      })
+      .catch((error) => alert(error.message));
+  };
+
+export const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredentials) => {
+        const { user } = userCredentials;
+        console.log('Logged in with:', user.email);
+        })
+        .catch((error) => alert(error.message));
+    };
+
+export const handleForgot = () => {
+    if (email === '') {
+        alert('Ingrese un correo');
+    } else {
+        alert(`Se ha enviado un correo a ${email} para reestablecer la contraseña`);
+        sendPasswordResetEmail(auth, email);
+    }
+    };
+
+export const signInWithGoogle = () => {
+    signInWithPopup(auth, new GoogleAuthProvider())
+        .then((userCredentials) => {
+        const { user } = userCredentials;
+        console.log('Logged in with:', user.email);
+        })
+        .catch((error) => alert(error.message));
+    };
+
+export const signInWithGithub = () => {
+    signInWithPopup(auth, new GithubAuthProvider())
+        .then((userCredentials) => {
+        const { user } = userCredentials;
+        console.log('Logged in with:', user.email);
+        })
+        .catch((error) => alert(error.message));
+    };
+
+export const signInWithFacebook = () => {
+    signInWithPopup(auth, new FacebookAuthProvider())
+        .then((userCredentials) => {
+        const { user } = userCredentials;
+        console.log('Logged in with:', user.email);
+        })
+        .catch((error) => alert(error.message));
+    };
--- checkyourlife2.0/src/tests/login.test.js
+++ checkyourlife2.0/src/tests/login.test.js
@@ -1,9 +1,8 @@
 /**
  * @jest-environment jsdom
  */
-import { render, screen } from '@testing-library/react';
+ import { render, cleanup, screen, fireEvent } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
-import { Router } from 'react-router-dom';
 import {createMemoryHistory} from 'history';
 import { shallow } from 'enzyme';
 import React from 'react';
@@ -15,6 +14,30 @@ import {
 } from 'firebase/auth';
 import { auth } from '../Services/firebase.js';
 import '@testing-library/jest-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { handleSignUp, handleLogin, handleForgot, signInWithGoogle, signInWithGithub, signInWithFacebook } from '../Services/Login';
+
+
+describe("App component testing", () => {
+  it("should be able to type email input field", () => {
+    const { getByTestId } = render ( <Router>
+      <Login />
+    </Router>);
+
+    fireEvent.change(getByTestId("email"), { target: { value: "esteban10052002@gmail.com" } });
+    expect(getByTestId("email").value).toBe("esteban10052002@gmail.com");
+  })
+
+  it("should be able to type password input field", () => {
+    const { getByTestId } = render ( <Router>
+      <Login />
+    </Router>);
+
+    fireEvent.change(getByTestId("password"), { target: { value: "#1234" } });
+    expect(getByTestId("password").value).toBe("#1234");
+  })
+})
+
 
 describe("<Login />", () => {
   it('render the page correctly', () => {
@@ -94,23 +117,24 @@ describe("<Login />", () => {
     }
   });
 
-  test('Test SignInwithUserAndPassword', () => {
+  
+  test('Test signInWithEmailAndPassword', () => {
 
     const user = null;
-
+    const initialTasks = handleSignUp()
     signInWithEmailAndPassword(auth, 'esteban10052002@gmail.com', '123456#')
-      .then((userCredentials) => {
-        user=userCredentials
-      })
-      .then(() => {
-        expect(user).toBe('hBWpwdLtFmbWRQgqLchFVdspNp43');
-      })
+    .then((userCredentials) => {
+    const { user } = userCredentials;
+    console.log('Logged in with:', user.email);
     })
+    .catch((error) => alert(error.message));
+  })
 
+    
     test('Test createUserWithEmailAndPassword', () => {
 
       const user = null;
-  
+      const initialTasks = handleSignUp()
       createUserWithEmailAndPassword(auth, 'esteban10052002@gmail.com', '123456#')
         .then((userCredentials) => {
           user=userCredentials
