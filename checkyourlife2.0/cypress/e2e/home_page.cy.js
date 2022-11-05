3373c5c71852950bdeb7263c312ffbdd4ac94e96
KennethGalvez <gal20079@uvg.edu.gt>
2022-11-05 16:43:24 -0600

    Pruebas de Sistemas

--- checkyourlife2.0/cypress/e2e/home_page.cy.js
+++ checkyourlife2.0/cypress/e2e/home_page.cy.js
@@ -1,4 +1,5 @@
-describe('The Home Page', () => {
+// Ejemplos de pruebas
+/* describe('The Home Page', () => {
   it('successfully loads', () => {
     cy.visit('http://localhost:3000'); // URL Aplicacion
 
@@ -10,5 +11,47 @@ describe('The Home Page', () => {
     cy.get('')
       .type('gal20079@uvg.edu.gt')
       .should('have.value', 'gal20079@uvg.edu.gt');
+  });
+});
+
+describe('The Login Page', () => {
+  beforeEach(() => {
+    // reset and seed the database prior to every test
+    cy.exec('npm run db:reset && npm run db:seed');
+
+    // seed a user in the DB that we can control from our tests
+    // assuming it generates a random password for us
+    cy.request('POST', '/test/seed/user', { username: 'jane.lane' })
+      .its('body')
+      .as('currentUser');
+  });
+
+  it('sets auth cookie when logging in via form submission', function () {
+    // destructuring assignment of the this.currentUser object
+    const { username, password } = this.currentUser;
+
+    cy.visit('/login');
+
+    cy.get('input[name=username]').type(username);
+
+    // {enter} causes the form to submit
+    cy.get('input[name=password]').type(`${password}{enter}`);
+
+    // we should be redirected to /dashboard
+    cy.url().should('include', '/dashboard');
+
+    // our auth cookie should be present
+    cy.getCookie('your-session-cookie').should('exist');
+
+    // UI should reflect this user being logged in
+    cy.get('h1').should('contain', 'jane.lane');
+  });
+}); */
+
+describe('Login tests', () => {
+  it('Login puede abrirse', () => {
+    cy.visit('http://localhost:3000'); // URL Aplicacion
+    // cy.contains('.profile');
+    // cy.get('form > a').click();
   });
 });
