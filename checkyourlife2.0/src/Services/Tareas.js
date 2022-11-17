dfadddb6ceb33fca6f659740e50e85919cc44e4e
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-11-16 21:10:17 -0600

    changes 2

--- checkyourlife2.0/src/Services/Tareas.js
+++ checkyourlife2.0/src/Services/Tareas.js
@@ -57,7 +57,7 @@ export const getData = async (u) => {
 export const addTask = async (user, name, description, beginDate, endDate) => {
   const docRef = doc(db, 'users', user.uid);
   if (name.length > 2) {
-    /* istanbul ignore next */
+    /* istanbul ignore next / */
     exp = (beginDate && endDate) ? [new Date(beginDate), new Date(endDate)] : [];
     /* istanbul ignore next */
     await updateDoc(docRef, {
