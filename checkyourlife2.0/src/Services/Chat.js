c5adc1fa3e52fc01a91a3493c4e64cf1a3cbcf3f
Natanael Girón <70552363+Nate20029@users.noreply.github.com>
2022-11-16 18:06:13 -0600

    fixing bug

--- checkyourlife2.0/src/Services/Chat.js
+++ checkyourlife2.0/src/Services/Chat.js
@@ -1,7 +1,7 @@
-import { getDoc, collection } from 'firebase/firestore';
+import { getDoc, collection, getDocs } from 'firebase/firestore';
 import { auth, db } from './firebase';
 
 export const getDataMessenger = async () => {
   const docRef = collection(db, 'usuarios');
-  const docSnap = await getDoc(docRef);
+  const docSnap = await getDocs(docRef);
 };
